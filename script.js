b0ea2c836864845b41d0cbfa7f606d21d713d2b0
Damian <dmusolino@ymail.com>
2022-09-28 21:31:18 +0100

    scripted all types of loops

--- script.js
+++ script.js
@@ -7,8 +7,39 @@ var toDoList = [
 
 ];
 
-for (var i = 0; i<toDoList.length; i++) {
+toDoList.forEach(function(todo,i){
 
-   toDoList[i] = toDoList[i] + "!"
-}
+    console.log(todo,i)
+})
+
+//for LOOP//
+
+// for (var i = 0; i<toDoList.length; i++) {
+
+//    toDoList[i] = toDoList[i] + "!"
+// }
+
+
+
+// WHILE LOOP//
+
+//  var counter =  0;
+//  while (counter < 10)
+ 
+//   {
+//     console.log(counter);
+//     counter++}
+
+//DO LOOP//
+
+// var counterTwo = 10;
+
+// do {
+//     console.log(counterTwo);
+//     counterTwo--;
+
+// } while (counterTwo>0);
+
+
+
 
